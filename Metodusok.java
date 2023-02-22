9c26dd6278284700144b2c3878500d6bace9ba7c
Dark2Knight7 <ducpeti27@gmail.com>
2023-02-22 15:42:03 +0100

    feladatok felosztása külön metódusokra.

--- Metodusok.java
+++ Metodusok.java
@@ -10,6 +10,7 @@ public class Metodusok {
         elsoFeladat();
         masodikFeladat();
         harmadikFeladat();
+        negyedikFeladat();
     }
 
     private static void elsoFeladat() {
@@ -20,15 +21,22 @@ public class Metodusok {
 
     private static void masodikFeladat() {
         int a = 3, b = 6;
-        int osszeg = ketSzamOsszeg(a, b);
+        int osszeg = osszeg(a, b);
         String s = String.format("%d + %d = %d", a, b, osszeg);
         kiir(s);
     }
-    
+
     private static void harmadikFeladat() {
-        int a = 3, b = 6, c = 7, d = 9;
-        int osszeg = negySzamOsszeg(a, b, c, d);
+        int a = 3, b = 6, c = 4, d = 8;
+        int osszeg = osszeg(a, osszeg(b, osszeg(c, d)));
         String s = String.format("%d + %d + %d + %d = %d", a, b, c, d, osszeg);
+        kiir(s);
+    }
+
+    private static void negyedikFeladat() {
+        int a = 3, b = 6, c = 4;
+        double gyok = Math.sqrt(osszeg(a, osszeg(b, c)));
+        String s = String.format("gyök(%d + %d + %d) = %.0f", a, b, c, gyok);
         kiir(s);
     }
 
@@ -39,14 +47,8 @@ public class Metodusok {
         }
         return osszeg;
     }
-    
-//    még nincs meg a 4. feladat
-    
-    private static int negySzamOsszeg(int a, int b, int c, int d) {
-        return a+b+c+d;
-    }
-    
-    private static int ketSzamOsszeg(int a, int b) {
+
+    private static int osszeg(int a, int b) {
         return a+b;
     }
     

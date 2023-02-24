17e7e6eb0b6d8b092ae6c8a5b145a54b3f73bb40
Dark2Knight7 <ducpeti27@gmail.com>
2023-02-24 12:02:40 +0100

    2023.02.24.-órai munka első feltöltése.

--- Equalizer.java
+++ Equalizer.java
@@ -1,5 +1,44 @@
 package metodusok;
 
+import java.util.Random;
+
 public class Equalizer {
     
+    public static Random rnd = new Random();
+    
+    public static void main(String[] args) {
+        eq_5_sor();
+    }
+    
+    private static void eq_5_sor() {
+        eq();
+        eq(12);
+        eq();
+        eq(8);
+        eq();
+    }
+    
+    private static void eq() {
+        int db = rnd.nextInt(3, 8);
+        String szin = "\u001B[45m";
+        for (int i = 0; i < db; i++) {
+            String s = String.format(szin + " ");
+            kiir(s);
+        }
+        System.out.println("");
+    }
+    
+    private static void eq(int hossz) {
+        int db = hossz;
+        String szin = "\u001B[45m";
+        for (int i = 0; i < db; i++) {
+            String s = String.format(szin + " ");
+            kiir(s);
+        }
+        System.out.println("");
+    }
+    
+    private static void kiir(String str){
+        System.out.println(str);
+    }
 }
